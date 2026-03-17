482afc00d27a601f5ebb55a64dc726e3a3a164d7
givanov3-ux <g.ivanov3@g.nsu.ru>
2026-03-17 16:42:07 +0700

    Update grid_io.py

--- grid_io.py
+++ grid_io.py
@@ -2,6 +2,7 @@ from dataclasses import replace
 
 import pygame as pg
 import random
+import json
 '''
 aka, role A
 Ok, how I see grid (like a matrix)
@@ -21,6 +22,7 @@ def create_empty_grid(rows: int, cols: int) -> list[list[int]]:
 
     return empty_frid
 
+
 def random_grid(rows: int, cols: int, prob: float = 0.5) -> list[list[int]]:
     """Заполняет сетку случайными значениями с заданной вероятностью жизни."""
     rand_grid = [random.choices([1, 0], [prob, 1 - prob], k = cols) 
@@ -28,11 +30,22 @@ def random_grid(rows: int, cols: int, prob: float = 0.5) -> list[list[int]]:
     
     return rand_grid
 
+
 def load_grid_from_file(filename: str) -> list[list[int]]:
     """Читает сетку из текстового файла. Формат файла обсуждается командой."""
+    with open(filename, 'r', encoding='utf-8') as data:
+        grid = json.load(data)
+        data.close()
+        return grid
+
 
 def save_grid_to_file(grid: list[list[int]], filename: str) -> None:
     """Сохраняет сетку в файл."""
+
+    with open(filename, 'w', encoding='utf-8') as data:
+        json.dump(grid, data)
+        data.close()
+
 
 def set_cell(grid: list[list[int]], row: int, col: int, value: int) -> list[list[int]]:
     """
