d4525147f40c237543ae023c356e24d2b93fca64
Mikhail Gordeev <miha_gord@mail.ru>
2026-03-24 19:44:11 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -25,16 +25,3 @@ color_themes = {
         'text': (50, 25, 0)
         }
     }
-
-
-control_keys = {
-    "SPACE": "Pause / Resume",
-    "UP": "Increase speed",
-    "DOWN": "Decrease speed",
-    "R": "Random grid",
-    "C": "Clear grid",
-    "S": "Save grid",
-    "L": "Load grid",
-    "Mouse click": "Toggle cell",
-    "ESC": "Exit"
-}
